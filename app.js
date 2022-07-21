f1b6e9d431c75442450be8fe112d83bae30305cd
ksamavati <kayvonsamavati@gmail.com>
2022-07-21 09:40:46 -0400

    push 1

---
+++ app.js
@@ -0,0 +1,18 @@
+var Car = function (maxSpeed, driver) {
+    this.maxSpeed = driver;
+    this.driver = driver;
+    this.drive = function (speed, time) {
+        console.log(speed * time);
+    };
+    this.logDriver = function () {
+        console.log("driver name is " + this.driver);
+    };
+}
+
+var myCar = new Car(70, "Ninja Man");
+var myCar2 = new Car(40, "Humpty Dumpty");
+var myCar3 = new Car(10, "Shaun");
+var myCar4 = new Car(90, "James Bond");
+
+myCar.drive(30, 5);
+myCar.logDriver();
